2c912f718c2a4639896f914c57960a8f1ec83863
Mark Woodbridge <1101318+mrw34@users.noreply.github.com>
2019-11-09 08:39:15 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="parkrun-to-sqlite",
-    version="0.2",
+    version="0.3",
     author="Mark Woodbridge",
     author_email="mark.woodbridge@cantab.net",
     description="Create a SQLite database containing your parkruns",
